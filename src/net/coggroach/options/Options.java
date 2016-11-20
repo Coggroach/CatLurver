32b9c3393102fa567b566034829de2c8e2a7d9e3
Coggroach <xpgear2@gmail.com>
2016-11-20 13:35:05 +0000

    Initial Commit

---
+++ src/net/coggroach/options/Options.java
@@ -0,0 +1,10 @@
+package net.coggroach.options;
+
+public class Options
+{	
+	public static final int DEFAULT_SOCKET_ID = 2550;
+	
+	public static int SOCKET_ID;
+	
+	public static final int STUDENT_ID = 12302939;
+}
